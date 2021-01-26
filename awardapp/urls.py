36c5e7f9672f20b6f75291df2a890d24ca2998f9
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-26 22:47:49 +0300

    Updated urls.py

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -1,24 +1,25 @@
-from django.urls import path, include 
+
 from . import views
+from django.urls import path, re_path , include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
-from rest_framework import routers
 from django.conf.urls.static import static
+from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('posts', views.PostViewSet)
-router.register('profile', views.ProfileViewSet)
+router= routers.DefaultRouter()
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
-    path('search/', views.search_project, name='search'),
-    path('account/', include('django.contrib.auth.urls')),
-    path('profile/<username>/settings', views.edit_profile, name='edit'),
-    path('profile/<username>/', views.profile, name='profile'),
-    path('api/', include(router.urls)),
-    path('<username>/profile', views.user_profile, name='userprofile'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('project/<post>', views.project, name='project'),
-    
+urlpatterns=[
+    path('', views.index, name='gram-landing'),
+    path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/',views.register, name='registration'),
+    path('search/', views.searchprofile, name='search'),
+    path('post/', views.PostCreateView, name='post-create'),
+    path('accounts/profile/',views.profile, name='profile'),
+    path('like/<int:pk>/', views.like_image, name='like_post'),
+    path('api/projects',views.ProjectList.as_view()),
+    path('api/profiles',views.ProfileList.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
