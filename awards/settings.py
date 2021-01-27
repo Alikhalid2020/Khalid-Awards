7876460575c7c77c8f991eb92b9d6aeec19d8f39
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-27 10:47:45 +0300

    Deployment to heroku

--- awards/settings.py
+++ awards/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-import os
+from pathlib import Path,os
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
